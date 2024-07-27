9c8654d18139bd6091765fafb1d25735460012e8
wmonteiro-test-devops <monteiro.wellington@pucpr.br>
2024-07-27 11:28:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-# 127.0.0.1:8000/teste1
-@app.get("/teste1")
+# 127.0.0.1:8000/teste
+@app.get("/teste")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
