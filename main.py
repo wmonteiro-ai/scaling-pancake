e0504a8b1371ec4909be2fbba92a82f97ec08246
wmonteiro-test-devops <monteiro.wellington@pucpr.br>
2024-07-27 11:19:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# 127.0.0.1:8000/
-@app.get("/")
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
-# 127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
